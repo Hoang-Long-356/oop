b059b6c17833fe1898c8acf790c894a7ae796bb8
Hoang-Long-356 <24020213@vnu.edu.vn>
2025-10-02 00:41:39 +0700

    HW1: add Introduction.java

---
+++ HW1/Introduction.java
@@ -0,0 +1,16 @@
+public class Introduction {
+    public static void main(String[] args) {
+
+        System.out.println(
+                "Võ Trần Hoàng Long" + "\t" + "24020213" + "\t" + "K69IT6" + "\t" + "Hoang-Long-356" + "\t" + "24020213@vnu.edu/vn"
+        );
+
+        for (int n = 9; n >= 1; n--) {
+            System.out.println(n + " bottles of beer on the wall, " + n + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
